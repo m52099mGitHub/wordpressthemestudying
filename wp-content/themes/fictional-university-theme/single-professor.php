b97d2777cc6d205e7d7eb04d038614cccc9fdc5d
ColinHong <m52099m@163.com>
2022-07-23 20:16:22 +0800

    no

--- wp-content/themes/fictional-university-theme/single-professor.php
+++ wp-content/themes/fictional-university-theme/single-professor.php
@@ -5,7 +5,8 @@
         the_post(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image"
-        style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['url']; ?>)"></div>
+        style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['url']; ?>)">
+    </div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
@@ -17,7 +18,7 @@
 
 
 <div class="container container--narrow page-section">
-    
+
 
     <div class="generic-content">
         <div class="row group">
